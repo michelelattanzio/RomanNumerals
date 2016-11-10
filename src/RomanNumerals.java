52708df53e8bd66dae28bf0c085907af1baedecc
Lattanzio Angelo Michele <michele.lattanzio.ml@gmail.com>
2016-11-10 16:21:21 +0100

    Implementata la prima Exception

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,30 @@
 
 public class RomanNumerals {
-	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+	public char convertToInteger(String romanNum) {
+		//I V X  L  C   D   M
+		//1 5 10 50 100 500 1000
 		
+		Integer out  = 0;
+		char[] romanNumberSplitted = romanNum.toCharArray();
+		try{
+			int I = 0;
+			int X = 0;
+			int C = 0;
+			int M = 0;
+			for(char lett: romanNumberSplitted){
+				if(lett == 'I') I++;
+				if(lett == 'X') X++;
+				if(lett == 'C') C++;
+				if(lett == 'M') M++;
+				
+				System.out.println(lett);
+				System.out.println(I);
+				if(I > 3 )
+					throw new Exception("Le lettere I, X, C, M possono essere scritte massimo 3 volte");
+			}
+		}catch(Exception e){
+			System.out.print(e.getMessage());
+			}
+		return romanNumberSplitted[0];		
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -1,12 +1,14 @@
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
 
 public class TestRomanNumerals {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		RomanNumerals test = new RomanNumerals();
+		assertEquals('I', test.convertToInteger("IIII"));
 	}
 
 }
